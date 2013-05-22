f3d9af734f885e6dd56f14fe24bba2bebc34b15e
ryosms <ryo.sms@gmail.com>
2013-05-22 23:29:13 +0900

    Add method for API that gets character(s) to interface
    キャラクター情報取得APIを叩くためのメソッドを追加

--- ImasdbAndroidExample/src/jp/sumasu/imasdb/example/ImasDbApis.java
+++ ImasdbAndroidExample/src/jp/sumasu/imasdb/example/ImasDbApis.java
@@ -1,6 +1,7 @@
 
 package jp.sumasu.imasdb.example;
 
+import jp.sumasu.imasdb.example.pojo.CharacterList;
 import jp.sumasu.imasdb.example.pojo.CharacterTypeList;
 
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
@@ -25,5 +26,15 @@ public interface ImasDbApis {
      */
     @Get("character/type/list?format=list")
     CharacterTypeList getCharacterTypeList();
+
+    /**
+     * キャラクターリスト取得
+     * 
+     * @param type
+     * @param includeProfile
+     * @return
+     */
+    @Get("character/list?type={type}&include_profile={includeProfile}")
+    CharacterList getCharacterList(int type, boolean includeProfile);
 
 }
